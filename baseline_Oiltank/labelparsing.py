6b24ffc8a8e6cc9405f4237f3ad4454847743bed
movie5 <oyh5800@naver.com>
2023-07-23 15:51:39 +0900

    Delete All

--- baseline_Oiltank/labelparsing.py
+++
@@ -1,7 +0,0 @@
-import json
-with open("OBJ05166_PS3_K3A_NIA0338.json", 'r') as f:
-    data = json.load(f)
-
-num = data['features'][1]["properties"]["object_imcoords"]
-parsed_numbers = [round(float(num), 2) for num in num.split(',')]
-print(parsed_numbers)
